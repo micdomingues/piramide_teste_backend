d7a802677828a4c640ea70511b66c2b2d5d855e8
Michael <micdomingues@hotmail.com>
2019-08-27 19:23:48 -0300

    Initial Commit

---
+++ src/main/java/br/com/michael/Piramide/de/Teste/PiramideDeTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.michael.Piramide.de.Teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiramideDeTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiramideDeTesteApplication.class, args);
+	}
+
+}
